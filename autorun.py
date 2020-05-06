1c085a01d0e5f2c943460aaae49a0d1db47709db
jjdelcerro <jjdelcerro@administrador-Lenovo-G50-70>
2020-05-06 10:22:56 +0200

    Atrapados errores del autorun para que no intente hacer nada si no estan instalados los plugins de los que depende.

--- autorun.py
+++ autorun.py
@@ -3,7 +3,16 @@ import gvsig
 from gvsig import getResource
 from java.io import File
 from org.gvsig.tools import ToolsLocator
-from addons.PointDensityGrid.pointDensityGridGeoprocess import PointDensityGridGeoprocess
+
+
+try:
+  from addons.PointDensityGrid.pointDensityGridGeoprocess import PointDensityGridGeoprocess
+except:
+  import sys
+  ex = sys.exc_info()[1]
+  gvsig.logger("Can't load module 'PointDensityGridGeoprocess'. " + str(ex), gvsig.LOGGER_WARN)#, ex)
+  PointDensityGridGeoprocess = None
+
 
 # Icon made by [author link] from www.flaticon.com
 
@@ -12,6 +21,8 @@ def i18nRegister():
     i18nManager.addResourceFamily("text",File(getResource(__file__,"i18n")))
   
 def main(*args):
+  if PointDensityGridGeoprocess== None:
+    return
   i18nRegister()
   process = PointDensityGridGeoprocess()
   process.selfregister("Scripting")
